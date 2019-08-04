ad30113bded368b516d3a01ab33747acc06791a0
PrinceOfAndroid <13627132919@163.com>
2019-08-04 22:48:13 +0800

    Aop权限申请代码添加

---
+++ baselib/src/main/java/com/base/baselib/permission/support/Default.java
@@ -0,0 +1,29 @@
+package com.base.baselib.permission.support;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+import com.base.baselib.permission.interf.ISetting;
+
+
+
+
+public class Default implements ISetting {
+
+    private Context context;
+
+    public Default(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public Intent getSetting() {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", context.getPackageName(), null));
+        return intent;
+    }
+}
